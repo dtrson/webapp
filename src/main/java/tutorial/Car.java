a040939afdaa47097ba0146492280805c97147c0
Son-Truong Duong <sduong@sama.int>
2017-08-01 10:02:19 +0200

    Testing with JUnit and Mockito

---
+++ src/main/java/tutorial/Car.java
@@ -0,0 +1,31 @@
+package tutorial;
+
+public class Car {
+	
+	private Engine engine;
+	private String warningMessage;
+	
+	public Car(Engine engine){
+		this.engine = engine;
+	}
+
+	public void accelerate(){
+		
+		engine.increaseRpm();
+		
+		if(engine.getRpm()>5000){
+			setWarningMessage("Slow Down!");
+		}
+	}
+	
+	public String getWarningMessage() {
+		return warningMessage;
+	}
+
+	public void setWarningMessage(String warningMessage) {
+		this.warningMessage = warningMessage;
+	}
+	
+	
+	
+}
